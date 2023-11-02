7e16ca2918a510ce2830275e9c89f1b5f229f7ca
Ryan Richardson <ryanrich811@gmail.com>
2023-11-02 17:03:46 -0400

    Generated gRPC files

--- Week1/QuadraticEquation.py
+++ Week1/QuadraticEquation.py
@@ -12,3 +12,6 @@ def quadraticEquation():
         x1 = (-b + math.sqrt((b**2) - (4 * a * c))) / (2 * a)
         x2 = (-b - math.sqrt((b**2) - (4 * a * c))) / (2 * a)
         return (str(x1), str(x2))
+
+
+# python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. QuadraticEquation.proto
