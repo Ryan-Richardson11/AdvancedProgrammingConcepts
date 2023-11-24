838ea4db903ae49db5fde2a1e4a73e2d6c2fcc9f
unknown <ryanrich811@gmail.com>
2023-11-24 11:26:07 -0500

    Set up full fetch api workflow

--- Week5/FoodFactAPI.js
+++ Week5/FoodFactAPI.js
@@ -1,3 +1,13 @@
-function getFoodInformation() {
-    var url = "https://world.openfoodfacts.org/api/v2/product/737628064502.json"
+function getFoodInformation(product) {
+    var url = "https://world.openfoodfacts.org/api/v2/" + product + "/737628064502.json"
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            diplayFoodInformation(data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
